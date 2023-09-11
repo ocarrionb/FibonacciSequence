0e3ef4932a929b47eb0239b14a94edd04d98d689
Octavio Carrion <octcarbar@gmail.com>
2023-09-11 13:25:28 -0600

    Add service layer and dependency injection
    Add service layer and dependency injection

--- Fibonacci/Program.cs
+++ Fibonacci/Program.cs
@@ -1,3 +1,5 @@
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Add services
+builder.Services.AddScoped<IFibonacciService, FibonacciService>();
 
 var app = builder.Build();
 
---
+++ Services/FibonacciService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public sealed class FibonacciService : IFibonacciService
+    {
+        public FibonacciService() { }
+        public async Task<int> FibonacciSequence(int index)
+        {
+            int val = 2;
+            return val;
+        }
+    }
+}
